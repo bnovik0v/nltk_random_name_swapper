7c3c6619e0b868e024750e7255a57359d7a9f3be
bnovikov <nvikv-brja@yandex.ru>
2021-03-04 13:12:44 +0300

    refactor: add description fix: output file names

--- src/run.py
+++ src/run.py
@@ -4,36 +4,40 @@ from random_person_swapper import RandomPersonSwapper
 
 
 def read_file(name):
+    """Read the content of file"""
     f = open(name)
     fd = f.read()
     f.close()
-
     return fd
 
 def write_file(src, name):
+    """Write the content in file"""
     f = open(name, 'w')
-    f.write(src)
-    f.close()
-
-def write_lines(src, name):
-    f = open(name, 'w')
-    f.writelines(str(line) + '\n' for line in src)
+    f.write(str(src))
     f.close()
 
 
 if __name__ == '__main__':
-    input_path = 'inout/' + sys.argv[1]
+    # Get input file name from command arg
+    file_name = sys.argv[1]
 
+    # Set inout path
+    inout_path = 'inout/'
+
+    # Read text from input file
+    input_path = inout_path + file_name
     text = read_file(input_path)
 
+    # Use RandomPersonSwapper to get transformed text and rules
     rps = RandomPersonSwapper()
+    new_text, rules = rps.make_replacement(text)
 
-    new_text, pairs = rps.make_replacement(text)
+    # Write the results in output files
+    write_file(new_text, f'{inout_path}out_{file_name}')
+    write_file(rules, f'{inout_path}rules_{file_name}')
 
-    write_file(new_text, f'inout/{sys.argv[1]}_out.txt')
-    write_lines(pairs, f'inout/{sys.argv[1]}_rules.txt')
-
-    print(pairs)
+    # Output the results on screen
+    print(rules)
     print(new_text)
 
 
